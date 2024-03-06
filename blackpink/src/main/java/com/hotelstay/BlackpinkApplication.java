409fe104cc93824edd8b3aba503d48e570a48ea8
hansamho <tkagh258@naver.com>
2024-03-06 15:44:39 +0900

    first

---
+++ blackpink/src/main/java/com/hotelstay/BlackpinkApplication.java
@@ -0,0 +1,13 @@
+package com.hotelstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackpinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackpinkApplication.class, args);
+	}
+
+}
---
+++ blackpink/src/test/java/com/hotelstay/BlackpinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackpinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
