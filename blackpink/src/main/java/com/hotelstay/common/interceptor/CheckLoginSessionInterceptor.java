cf826eb980098265919fe7c492e2d49b5e471feb
hansamho <tkagh258@naver.com>
2024-05-08 14:04:12 +0900

    usr_hotel

--- blackpink/src/main/java/com/hotelstay/common/interceptor/CheckLoginSessionInterceptor.java
+++ blackpink/src/main/java/com/hotelstay/common/interceptor/CheckLoginSessionInterceptor.java
@@ -9,19 +9,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CheckLoginSessionInterceptor implements HandlerInterceptor {
 
-//	@Override
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception {
-//		
-//		if (request.getSession().getAttribute("sessSeqUsr") != null) {
-//			// by pass
-//		} else {
-//			response.sendRedirect(Constants.URL_LOGINFORM);
-//	        return false;
-//		} 
-//		
-//		return HandlerInterceptor.super.preHandle(request, response, handler);
-//	}
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		if (request.getSession().getAttribute("sessSeqUsr") != null) {
+			// by pass
+		} else {
+			response.sendRedirect(Constants.URL_LOGINFORM);
+	        return false;
+		} 
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
 	
 //	public boolean preHandleAdm(HttpServletRequest request, HttpServletResponse response, Object handler)
 //			throws Exception {
